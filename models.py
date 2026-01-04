330fc4d0429d7b1f6efa47d221bd49cfeba28b62
ipekfurkan981 <132554320+furkanipek190@users.noreply.github.com>
2026-01-05 00:28:58 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,15 @@
+"""
+models.py
+---------
+Contains the definition of the data structures used in the application.
+Note: Currently, the model is for conceptual purposes only.
+"""
+
 from dataclasses import dataclass
 
 @dataclass
 class Note:
     id: int
     text: str
+
     created_at: str
